c9471ec498a3163fd5b90300e6e6bf016a51fab4
treth <treth@DESKTOP-NL3N9KH.domain>
2018-12-19 22:00:29 -0700

    Added system out text string

---
+++ src/edu/cnm/deepdive/helloworld/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package edu.cnm.deepdive.helloworld;
+
+/**
+ * Prints "Hello World@" to standard output.
+ *
+ * @param args      Comman-Line parameters (ignored).
+ */
+public class HelloWorld {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+
+  }
+
+}
